bd3bf8d5d3dfc7e8327f401ba7cb7e05c556fa31
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-04-12 20:30:41 +0100

    Delete native directory

--- native/src/main/kotlin/me/rhunk/snapenhance/nativelib/NativeConfig.kt
+++
@@ -1,7 +0,0 @@
-package me.rhunk.snapenhance.nativelib
-
-data class NativeConfig(
-    val disableBitmoji: Boolean = false,
-    val disableMetrics: Boolean = false,
-    val hookAssetOpen: Boolean = false,
-)
