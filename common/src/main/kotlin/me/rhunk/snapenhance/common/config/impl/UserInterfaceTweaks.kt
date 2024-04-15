c2c5b86ba9a6c501dc4ff1cecd8ef6e536b50871
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-04-15 12:35:05 +0100

    refactor

--- common/src/main/kotlin/me/rhunk/snapenhance/common/config/impl/UserInterfaceTweaks.kt
+++ common/src/main/kotlin/me/rhunk/snapenhance/common/config/impl/UserInterfaceTweaks.kt
@@ -29,9 +29,9 @@ class UserInterfaceTweaks : ConfigContainer() {
         val actionMenuBackgroundColour = string("action_menu_background_colour") { inputCheck = checkInputColor }
         val actionMenuRoundBackgroundColour = string("action_menu_round_background_colour") { inputCheck = checkInputColor }
         val chatColour = string("chat_colour") { inputCheck = checkInputColor }
-        val snapColour = string("snap_colour") { inputCheck = checkInputColour }
-        val baseColour = string("base_colour") { inputCheck = checkInputColour } 
-        val iconColour = string("icon_colour") { inputCheck = checkInputColour }
+        val snapColour = string("snap_colour") { inputCheck = checkInputColor }
+        val baseColour = string("base_colour") { inputCheck = checkInputColor } 
+        val iconColour = string("icon_colour") { inputCheck = checkInputColor }
     }
 
     val friendFeedMenuButtons = multiple(
