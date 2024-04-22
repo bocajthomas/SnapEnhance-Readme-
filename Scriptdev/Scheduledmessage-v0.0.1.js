b6604e4e8f4532ae2e5b0f549abd077909d7dad4
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-04-23 00:49:50 +0100

    Update Scheduledmessage-v0.0.1.js

--- Scriptdev/Scheduledmessage-v0.0.1.js
+++ Scriptdev/Scheduledmessage-v0.0.1.js
@@ -16,81 +16,75 @@ var hooker = require("hooker");
 var events = require("events");
 
 (function () {
-    'use strict';
-    var conversationToolboxContext = {
-        events: [],
-    };
-
-    // Replace Inside " " with your custom message
-    var inputMessage = "Input You're Text Here";
-    // Replace Inside " " with your Time preference 
-    var time = "2000";
+	'use strict';
+	var conversationToolboxContext = {
+		events: [],
+	};
 	
-    function createConversationToolboxUI() {
-        conversationToolboxContext.events.push({
-            start: function (builder, args) {
-                builder.button("Send Message", function () {
-                    var conversationId = args["conversationId"];
-			sleep(time)
-			messaging.sendChatMessage(conversationId, inputMessage, function () { });
-		});
-	    });
-    };
-
-				
-    var snapActivityContext = {
+	// Replace Inside " " with your custom messag
+	var inputMessage = "Input You're Text Here";
+	// Replace Inside " " with your Time preference
+	var time = "2000";
+	
+	function createConversationToolboxUI() {
+		conversationToolboxContext.events.push({
+			start: function (builder, args) {
+				builder.button("Send Message", function () {
+					var conversationId = args["conversationId"];
+					sleep(time)
+						messaging.sendChatMessage(conversationId, inputMessage, function () { });
+				});
+			});
+	};
+	
+	var snapActivityContext = {
         activity: null,
         events: [],
-    };
-
-    function start(_a) {
+	};
+	
+	function start(_a) {
         _a.snapActivityContext; _a.conversationToolboxContext; _a.settingsContext;
         createInterface();
-    }
-
-    function createInterface() {
+	}
+	
+	function createInterface() {
         createConversationToolboxUI();
-    }
-
-    var snapApplicationContext = {
+	}
+	
+	var snapApplicationContext = {
         context: null,
         events: [],
-    };
-
-    var snapEnhancerContext = {
+	};
+	
+	var snapEnhancerContext = {
         context: null,
         events: [],
-    };
-
-    var unloadContext = {
+	};
+	
+	var unloadContext = {
         events: [],
-    };
-
-    start({
-        snapActivityContext: snapActivityContext,
-        snapApplicationContext: snapApplicationContext,
-        snapEnhancerContext: snapEnhancerContext,
-        unloadContext: unloadContext,
-        conversationToolboxContext: conversationToolboxContext,
-    });
-
-    module.onSnapMainActivityCreate = function (activity) {
-        snapActivityContext.activity = activity;
-        snapActivityContext.events.forEach(function (event) {
-            event.start(activity, null);
-        });
-    };
-
-    module.onUnload = function () {
-        unloadContext.events.forEach(function (event) {
-            event.start(null, null);
-        });
-    };
-
-    im.create("conversationToolbox", function (builder, args) {
-        conversationToolboxContext.events.forEach(function (event) {
-            event.start(builder, args);
-        });
-    });
-
+	};
+	start({
+		snapActivityContext: snapActivityContext,
+		snapApplicationContext: snapApplicationContext,
+		snapEnhancerContext: snapEnhancerContext,
+		unloadContext: unloadContext,
+		conversationToolboxContext: conversationToolboxContext,
+	});
+	module.onSnapMainActivityCreate = function (activity) {
+		snapActivityContext.activity = activity;
+		snapActivityContext.events.forEach(function (event) {
+			event.start(activity, null);
+		});
+	};
+	module.onUnload = function () {
+		unloadContext.events.forEach(function (event) {
+			event.start(null, null);
+		});
+	};
+	im.create("conversationToolbox", function (builder, args) {
+		conversationToolboxContext.events.forEach(function (event) {
+			event.start(builder, args);
+		});
+	});
 })();
