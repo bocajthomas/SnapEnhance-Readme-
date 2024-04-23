1042669f045e7fd346681769f31a27fef036992c
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-04-23 01:27:55 +0100

    Refactor & Fixes

---
+++ Scriptdev/Scheduledmessage-v0.1.js
@@ -0,0 +1,94 @@
+// ==SE_module==
+// name: scheduled_message 
+// displayName: Scheduled Message 
+// description: A Script That Allows For Scheduling Messages 
+// version: 0.1
+// author: Jacob Thomas 
+// ==/SE_module==
+
+var networking = require("networking");
+var messaging = require("messaging");
+var config = require("config");
+var im = require("interface-manager");
+var ipc = require("ipc");
+var javaInterfaces = require("java-interfaces");
+var hooker = require("hooker");
+var events = require("events");
+
+(function () {
+    'use strict';
+    var conversationToolboxContext = {
+        events: [],
+    };
+
+    // Replace Inside " " With You're Custom Message
+    var inputMessage = "Jacob Thomas Aka Bocajthomas";
+
+    function createConversationToolboxUI() {
+        conversationToolboxContext.events.push({
+            start: function (builder, args) {
+                builder.button("Send Message", function () {
+                    var conversationId = args["conversationId"];
+			sleep(2000)
+			messaging.sendChatMessage(conversationId, inputMessage, function () { });
+		});
+	    }
+	});
+    }
+				
+    var snapActivityContext = {
+        activity: null,
+        events: [],
+    };
+
+    function start(_a) {
+        _a.snapActivityContext; _a.conversationToolboxContext; _a.settingsContext;
+        createInterface();
+    }
+
+    function createInterface() {
+        createConversationToolboxUI();
+    }
+
+    var snapApplicationContext = {
+        context: null,
+        events: [],
+    };
+
+    var snapEnhancerContext = {
+        context: null,
+        events: [],
+    };
+
+    var unloadContext = {
+        events: [],
+    };
+
+    start({
+        snapActivityContext: snapActivityContext,
+        snapApplicationContext: snapApplicationContext,
+        snapEnhancerContext: snapEnhancerContext,
+        unloadContext: unloadContext,
+        conversationToolboxContext: conversationToolboxContext,
+    });
+
+    module.onSnapMainActivityCreate = function (activity) {
+        snapActivityContext.activity = activity;
+        snapActivityContext.events.forEach(function (event) {
+            event.start(activity, null);
+        });
+    };
+
+    module.onUnload = function () {
+        unloadContext.events.forEach(function (event) {
+            event.start(null, null);
+        });
+    };
+
+    im.create("conversationToolbox", function (builder, args) {
+        conversationToolboxContext.events.forEach(function (event) {
+            event.start(builder, args);
+        });
+    });
+
+})();
