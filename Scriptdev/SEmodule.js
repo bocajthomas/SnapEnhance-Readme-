a3d0f2b4684ce7346980a6acba11afbc999121c5
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-04-24 10:33:13 +0100

    Update SEmodule.js

--- Scriptdev/SEmodule.js
+++ Scriptdev/SEmodule.js
@@ -5,3 +5,13 @@
 // version: 0.1
 // author: Jacob Thomas 
 // ==/SE_module==
+
+var networking = require("networking");
+var messaging = require("messaging");
+var config = require("config");
+var im = require("interface-manager");
+var ipc = require("ipc");
+var javaInterfaces = require("java-interfaces");
+var hooker = require("hooker");
+var events = require("events");
+
