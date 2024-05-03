fb16a7bcb7180e940d5838e897b817ae24b6cc64
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-05-03 17:20:59 +0100

    Add files via upload

---
+++ Scriptdev/CustomToast v2.js
@@ -0,0 +1,11 @@
+// ==SE_module==
+// name: custom_toast
+// displayName: Custom Toast
+// description: A Script that shows a custom toast on the startup of Snapchat.
+// version: 0.2
+// author: Gabe Modz 
+// ==/SE_module==
+
+module.onSnapMainActivityCreate = activity => {
+    shortToast("Welcome back Gabriel")
+}
