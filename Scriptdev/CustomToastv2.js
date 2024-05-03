290112a6dac81d4a2933171fa04a1ac4a10b24f6
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-05-03 18:23:59 +0100

    fix: Settings

--- Scriptdev/CustomToastv2.js
+++ Scriptdev/CustomToastv2.js
@@ -16,20 +16,18 @@ var hooker = require("hooker");
 var events = require("events");
 
 var defaultPrompt = "Test";
-    function createManagerToolBoxUI() {
-        settingsContext.events.push({
-            start: function (builder) {
-                builder.row(function (builder) {
-                    builder
-                        .textInput("Custom Prompt", config.get("customPrompt", defaultPrompt), function (value) {
-                        config.set("customPrompt", value, true);
-                    })
-                        .maxLines(8)
-                        .singleLine(false);
-                });
-            },
-        });
-    }
+function createManagerToolBoxUI() {
+    settingsContext.events.push({
+        start: function (builder) {
+            builder.row(function (builder) {
+                builder.textInput("Custom Prompt", config.get("customPrompt", defaultPrompt), function (value) {
+                    config.set("customPrompt", value, true);
+                }) .maxLines(8)
+                    .singleLine(false);
+            });
+        },
+    });
+}
 
 module.onSnapMainActivityCreate = activity => {
     shortToast("Welcome back Gabriel")
