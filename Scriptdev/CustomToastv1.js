1f8a33c4a9870d84a86a3b1f8d3734a5972db664
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-05-04 23:26:39 +0100

    Update CustomToastv1.js

--- Scriptdev/CustomToastv1.js
+++ Scriptdev/CustomToastv1.js
@@ -35,7 +35,7 @@ function createManagerToolBoxUI() {
 
 // TODO: fix error message "org.mozilla.javascript.NativeJavaObject@7277a10" when toast is shown  
 module.onSnapMainActivityCreate = activity => {
-        const customPrompt = config.get("customPrompt", defaultPrompt).toString();
+        const customPrompt = String(config.get("customPrompt"));
         shortToast(customPrompt);
 }
 function createInterface() {
