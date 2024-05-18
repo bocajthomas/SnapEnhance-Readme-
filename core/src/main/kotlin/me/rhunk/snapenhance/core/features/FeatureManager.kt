255be64e414f72567ddcf735a4b87fb4c3d4e9e9
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-05-18 13:52:19 +0100

    Update FeatureManager.kt

--- core/src/main/kotlin/me/rhunk/snapenhance/core/features/FeatureManager.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/core/features/FeatureManager.kt
@@ -128,6 +128,7 @@ class FeatureManager(
             ComposerHooks(),
             DisableCustomTabs(),
             BestFriendPinning(),
+            UnlimitedMultiSnap(),
         )
         initializeFeatures()
     }
