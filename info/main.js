f225f993d6c93ffed7e1f8539affd78da7527612
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-06-15 01:37:31 +0100

    chore: add main

---
+++ info/main.js
@@ -0,0 +1,16 @@
+// ==SE_module==
+// name: 
+// displayName: 
+// description: A Script 
+// version: 0.1
+// author: Jacob Thomas 
+// ==/SE_module==
+
+var networking = require("networking");
+var messaging = require("messaging");
+var config = require("config");
+var im = require("interface-manager");
+var ipc = require("ipc");
+var javaInterfaces = require("java-interfaces");
+var hooker = require("hooker");
+var events = require("events");
