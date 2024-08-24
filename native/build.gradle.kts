abf25ad441e4f100e85a89f1b630460b588ffe8a
Jacob <41988041+bocajthomas@users.noreply.github.com>
2024-08-24 17:34:59 +0100

    fix(build): native ndk version
    - reverted back to v26.3.11579264

--- native/build.gradle.kts
+++ native/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     namespace = rootProject.ext["applicationId"].toString() + ".nativelib"
     compileSdk = 34
     buildToolsVersion = "34.0.0"
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "26.3.11579264"
 
     buildFeatures {
         buildConfig = true
