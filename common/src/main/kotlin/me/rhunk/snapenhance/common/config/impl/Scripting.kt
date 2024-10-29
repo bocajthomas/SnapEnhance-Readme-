81c19cc7d12c1ab0898766308867aa02d18b5fb1
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2024-10-29 23:05:59 +0000

    feat(scripting): add version check

--- common/src/main/kotlin/me/rhunk/snapenhance/common/config/impl/Scripting.kt
+++ common/src/main/kotlin/me/rhunk/snapenhance/common/config/impl/Scripting.kt
@@ -2,12 +2,13 @@ package me.rhunk.snapenhance.common.config.impl
 
 import me.rhunk.snapenhance.common.config.ConfigContainer
 import me.rhunk.snapenhance.common.config.ConfigFlag
+import me.rhunk.snapenhance.common.config.RES_OBF_VERSION_CHECK
 
 class Scripting : ConfigContainer() {
     val developerMode = boolean("developer_mode", false) { requireRestart() }
     val moduleFolder = string("module_folder", "modules") { addFlags(ConfigFlag.FOLDER, ConfigFlag.SENSITIVE); requireRestart()  }
     val autoReload = unique("auto_reload", "snapchat_only", "all")
-    val integratedUI = boolean("integrated_ui", false) { requireRestart() }
-    val oldToolBoxAddView = boolean("old_tool_box_add_view", false) { requireRestart() }
+    val integratedUI = boolean("integrated_ui", false) { requireRestart(); versionCheck = RES_OBF_VERSION_CHECK.copy(isDisabled = true)  }
+    val oldToolBoxAddView = boolean("old_tool_box_add_view", false) { requireRestart(); versionCheck = RES_OBF_VERSION_CHECK.copy(isDisabled = true)  }
     val disableLogAnonymization = boolean("disable_log_anonymization", false) { requireRestart() }
 }
