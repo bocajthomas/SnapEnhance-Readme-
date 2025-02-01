c63dff84ba16f6779a6106dd078abf108fa68267
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2025-02-01 14:09:46 +0000

    refactor(quick_actions): icons - Quick Actions now use rounded icons for UX

--- app/src/main/kotlin/me/rhunk/snapenhance/action/EnumQuickActions.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/action/EnumQuickActions.kt
@@ -1,10 +1,10 @@
 package me.rhunk.snapenhance.action
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FolderOpen
-import androidx.compose.material.icons.filled.History
-import androidx.compose.material.icons.filled.Palette
-import androidx.compose.material.icons.filled.PersonSearch
+import androidx.compose.material.icons.rounded.FolderOpen
+import androidx.compose.material.icons.rounded.History
+import androidx.compose.material.icons.rounded.Palette
+import androidx.compose.material.icons.rounded.PersonSearch
 import androidx.compose.ui.graphics.vector.ImageVector
 import me.rhunk.snapenhance.ui.manager.Routes
 
@@ -13,16 +13,16 @@ enum class EnumQuickActions(
     val icon: ImageVector,
     val action: Routes.() -> Unit
 ) {
-    FILE_IMPORTS("file_imports", Icons.Default.FolderOpen, {
+    FILE_IMPORTS("file_imports", Icons.Rounded.FolderOpen, {
         fileImports.navigateReset()
     }),
-    FRIEND_TRACKER("friend_tracker", Icons.Default.PersonSearch, {
+    FRIEND_TRACKER("friend_tracker", Icons.Rounded.PersonSearch, {
         friendTracker.navigateReset()
     }),
-    LOGGER_HISTORY("logger_history", Icons.Default.History, {
+    LOGGER_HISTORY("logger_history", Icons.Rounded.History, {
         loggerHistory.navigateReset()
     }),
-    THEMING("theming", Icons.Default.Palette, {
+    THEMING("theming", Icons.Rounded.Palette, {
         theming.navigateReset()
     })
 }
